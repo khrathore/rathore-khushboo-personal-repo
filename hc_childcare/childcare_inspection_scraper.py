4453f4b10a50dbaee850301da9215d985d7b8d55
khrathore <44534747+khrathore@users.noreply.github.com>
2023-06-07 09:14:32 -0400

    Update childcare_inspection_scraper.py

--- hc_childcare/childcare_inspection_scraper.py
+++ hc_childcare/childcare_inspection_scraper.py
@@ -16,9 +16,6 @@ list_of_rows = []
 for row in table.find_all('tr'):
     list_of_cells = []
     for cell in row.find_all('td'):
-        if cell.find('a'):
-            
-        text = cell.text.strip()
         list_of_cells.append(text)
     list_of_rows.append(list_of_cells)
 
@@ -26,14 +23,43 @@ print(list_of_rows)
 
 #cell.text.find('a')['href']
 
-import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
-import requests_html as r_html
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-session = r_html.HTMLSession()
+driver = webdriver.Chrome()
 
-deep_url = 'https://www.checkccmd.org/FacilityDetail.aspx?ft=&fn=&sn=&z=&c=&co=&fi=463466'
-response_deep = session.r_html.get(deep_url)
-table_deep = response_deep.r_html.find_all("li")
+response_deep = driver.get('https://www.checkccmd.org/default.aspx')
+driver.find_element(By.PARTIAL_LINK_TEXT, "view all open providers").click()
+driver.implicitly_wait(5)
+
+soup = BeautifulSoup(driver.page_source, 'lxml')
+
+list_of_rows = []
+for row in table.find_all('tr'):
+    list_of_cells = []
+    for cell in row.find_all('td'):
+        list_of_cells.append(cell.text)
+    list_of_rows.append(list_of_cells)
+
+print(table.prettify())
+
+list_of_rows = []
+for row in table.find_all('tr'):
+    list_of_cells = []
+    for cell in row.find_all('td'):
+        text = cell.text.strip()
+        list_of_cells.append(text)
+    list_of_rows.append(list_of_cells)
+
+print(list_of_rows)
+
+#print(response_deep)
+html_deep = response_deep.html.find('li', first=True)
+pprint(html_deep.text)
+
+soup_deep = BeautifulSoup(html_deep, features="html.parser")
+table_deep = soup_deep.find_all("li")
 pprint(table_deep)
