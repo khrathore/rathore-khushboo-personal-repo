e6800f6e28061de9c82fb90a69c80b16057cee4d
khrathore <44534747+khrathore@users.noreply.github.com>
2023-06-30 18:11:48 -0400

    Update childcare_inspection_scraper.py

--- hc_childcare/childcare_inspection_scraper.py
+++ hc_childcare/childcare_inspection_scraper.py
@@ -15,27 +15,15 @@ driver.get(url)
 page_source = driver.page_source
 soup = BeautifulSoup(page_source, 'html.parser')
 
-# get the listings
-table = soup.find('tbody')
+#Scrape, then click on each link and collect details, then hit back button and go to next
+
 list_of_rows = []
-# skip the first row because it's a header
-for row in table.find_all('tr')[1:]:
-    list_of_cells = []
-    for cell in row.find_all('td'):
-        if cell.find('a'):
-                list_of_cells.append(cell.text.strip())
-                list_of_cells.append('https://www.checkccmd.org/'+cell.find('a')['href'])
-                detail_page = 'https://www.checkccmd.org/'+cell.find('a')['href']
-        elif cell.text.strip() != '':
-            list_of_cells.append(cell.text.strip())
-        driver.find_element(By.XPATH, )
-    list_of_rows.append(list_of_cells)
-
-
-for index, page in enumerate(range(2,501)):
-    print(page)
+detail_rows = [] 
+for index, page in enumerate(range(1,2)):
     if page == 8:
         next_page = driver.find_element(By.LINK_TEXT, '...').click()
+    elif page == 1:
+        page = 1
     elif page % 7 == 1:
         next_page = driver.find_elements(By.LINK_TEXT, '...')[1].click()
     else:
@@ -49,10 +37,40 @@ for index, page in enumerate(range(2,501)):
             if cell.find('a'):
                 list_of_cells.append(cell.text.strip())
                 list_of_cells.append('https://www.checkccmd.org/'+cell.find('a')['href'])
-                driver.execute_script('''window.open("https://www.checkccmd.org/'+cell.find('a')['href']", "_blank");''')
             elif cell.text.strip() != '':
                 list_of_cells.append(cell.text.strip())
         list_of_rows.append(list_of_cells)
+    a_tags = driver.find_elements(By.TAG_NAME, "a")
+    facilities = [x for x in a_tags if "FacilityDetail" in x.get_attribute('href')]
+    #for facility in facilities:
+    facilities[1].click()
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'html.parser')
+    detail_lists = (soup.find('ul'))[1:2]
+    for row in table.find_all('li')[1:]:
+        list_of_cells = []
+        for cell in row.find_all('MainContent_txt'):
+            if cell.text.strip() != '':
+                list_of_cells.append(cell.text.strip())
+        list_of_rows.append(list_of_cells)
+    print(driver.current_url)
 
 pprint(list_of_rows)
 
+
+# get the listings
+'''table = soup.find('tbody')
+# skip the first row because it's a header
+for row in table.find_all('tr')[1:]:
+    list_of_cells = []
+    for cell in row.find_all('td'):
+        if cell.find('a'):
+                list_of_cells.append(cell.text.strip())
+                list_of_cells.append('https://www.checkccmd.org/'+cell.find('a')['href'])
+                detail_page = 'https://www.checkccmd.org/'+cell.find('a')['href']
+        elif cell.text.strip() != '':
+            list_of_cells.append(cell.text.strip())
+        a_tags = driver.find_element(By.TAG_NAME, "a")
+        facilities = [x for x in a_tags if "FacilityDetail" in x.get_attribute('href')]
+        
+    list_of_rows.append(list_of_cells)'''
