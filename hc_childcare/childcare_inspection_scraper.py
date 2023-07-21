f2d2d3055edf285eaac925e9901dc2caca9a5cb8
khrathore <44534747+khrathore@users.noreply.github.com>
2023-07-21 13:16:19 -0400

    Update childcare_inspection_scraper.py

--- hc_childcare/childcare_inspection_scraper.py
+++ hc_childcare/childcare_inspection_scraper.py
@@ -43,38 +43,30 @@ for index, page in enumerate(range(1,2)):
         list_of_rows.append(list_of_cells)
     a_tags = driver.find_elements(By.TAG_NAME, "a")
     facilities = [x for x in a_tags if "FacilityDetail" in x.get_attribute('href')]
-    #for facility in facilities:
-    facilities[1].click()
-    page_source = driver.page_source
-    soup = BeautifulSoup(page_source, 'html.parser')
-    detail_lists = (soup.find_all('ul'))[1:2]
-    # get the information for each place
-    element_value = re.compile('MainContent_txt')
-    for entry in detail_lists:
+    for facility in facilities:
+        facility.click()
         dict_facility = {}
-        if entry.contains('labelForm'):
-            key = entry.text.strip()
-        elif entry.contains(element_value):
-            value = entry.text.strip()
-            list_of_cells.append(cell.text.strip())
-        detail_rows.append(list_of_cells)
+        page_source = driver.page_source
+        soup = BeautifulSoup(page_source, 'html.parser')
+        detail_lists = (soup.find_all('ul'))[0:2]
+        # get the information for each place
+        for columns in detail_lists:
+            entries = columns.find_all('li')      
+            for entry in entries:
+                #entry = entries[0]
+                if entry.find(class_='Excelslogo'):
+                    key = "Maryland Excels Level"
+                    value = entry.find(class_ = "detailLevelText").text.strip()
+                    dict_facility[key] = value
+                elif entry.text.strip() != '':
+                    key = entry.find(class_='labelForm').text.strip()
+                    value = entry.find(class_='detailText').text.strip()
+                    dict_facility[key] = value
+                else:
+                    next
+        detail_rows.append(dict_facility)
+        soup.find(id_="MainContent_LinkBack").click()
+        
 
 pprint(list_of_rows)
-
-
-# get the listings
-'''table = soup.find('tbody')
-# skip the first row because it's a header
-for row in table.find_all('tr')[1:]:
-    list_of_cells = []
-    for cell in row.find_all('td'):
-        if cell.find('a'):
-                list_of_cells.append(cell.text.strip())
-                list_of_cells.append('https://www.checkccmd.org/'+cell.find('a')['href'])
-                detail_page = 'https://www.checkccmd.org/'+cell.find('a')['href']
-        elif cell.text.strip() != '':
-            list_of_cells.append(cell.text.strip())
-        a_tags = driver.find_element(By.TAG_NAME, "a")
-        facilities = [x for x in a_tags if "FacilityDetail" in x.get_attribute('href')]
-        
-    list_of_rows.append(list_of_cells)'''
+print(detail_rows)
