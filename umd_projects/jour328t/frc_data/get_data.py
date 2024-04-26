be497ac8f6ba8ed3e8e714c369a0d9dec1b6bf59
khrathore <44534747+khrathore@users.noreply.github.com>
2024-04-26 18:38:38 +0000

    good json formatting

--- umd_projects/jour328t/frc_data/get_data.py
+++ umd_projects/jour328t/frc_data/get_data.py
@@ -1,7 +1,40 @@
 import requests
+import os
+import tbaapiv3client
+import yaml
+import pprint
+import json
 
-auth = "baYm46XIygSKHAFTWM9N5AORLQHikaOOx4QB8ZGlOx9Q5Z8Mq5fz7GhwUn0vMuWl"
+base_url = "https://www.thebluealliance.com/api/v3"
 
 headers = {
-    'Authorization': auth
+    'X-TBA-Auth-Key': os.environ.get('TBA_API')
 }
+
+years_list = [year for year in range(1992, 2025)]
+
+with open('frc_events.json', 'w') as file:
+    file.write('[\n')
+    for year in years_list:
+        url = base_url + "/events/" + str(year)
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            # Request was successful
+            events_data = response.json()
+            # Process events_data
+        for event in events_data:
+                json.dump(event, file)
+                if (year == 2024) & (event == events_data[len(events_data) - 1]):
+                     file.write("\n")
+                else:
+                    file.write(",\n")
+    file.write(']')
+
+
+
+
+
+
+
+
+
