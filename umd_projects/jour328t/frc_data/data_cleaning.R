429512e8ef8aade5a6881ada1826549382ca2163
khrathore <44534747+khrathore@users.noreply.github.com>
2024-05-01 22:56:07 -0400

    data cleaning

--- umd_projects/jour328t/frc_data/data_cleaning.R
+++ umd_projects/jour328t/frc_data/data_cleaning.R
@@ -5,11 +5,10 @@ library(tidyverse)
 library(jsonlite)
 library(dplyr)
 
-column_names <- c("team_key", "alliance_color", "comp_level", "event_key", "key", "winning_alliance", "blue_score", "red_scoare")
-data_by_team <- data.frame(matrix(ncol = length(column_names), nrow = 0))
 
-#matchdata <- 2
+
 expand_df <- function(matchdata) {
+  match_data <- data.frame(matrix(ncol = length(column_names), nrow = 0))
   match_info <- matches_clean[matchdata, ]
   red_teams <- str_split(match_info$alliances.red.team_keys[[1]], " ")
   blue_teams <- str_split(match_info$alliances.blue.team_keys[[1]], " ")
@@ -24,18 +23,21 @@ expand_df <- function(matchdata) {
     alliance_color <- colors[n]
     data <- data.frame(c(team_key = team_key, alliance_color = alliance_color, match_info['comp_level'], match_info['event_key'], match_info['key'], match_info['winning_alliance'], match_info['alliances.blue.score'], match_info['alliances.red.score'])) %>% 
       rename(blue_score = alliances.blue.score, red_score = alliances.red.score)
-    data_by_team <- rbind(data_by_team, data)
+    match_data <- rbind(match_data, data)
   }
-  
+  return(match_data)
 }
 
 current_teams <- fromJSON("frc_allcurrent.json")
 all_teams <- fromJSON("frc_allteams.json")
+all_events <- fromJSON("frc_events.json")
 
 dead_teams <- all_teams %>% 
   anti_join(current_teams)
 
 write_json(dead_teams, "frc_deadteams.json")
+
+team_ends <- fromJSON("frc_endyears.json")
 
 all_matches <- fromJSON("event_results.json")
 
@@ -44,13 +46,17 @@ matches_clean <- all_matches %>%
   flatten() %>% 
   select(comp_level, event_key, key, winning_alliance, alliances.blue.score, alliances.blue.team_keys, alliances.red.score, alliances.red.team_keys)
 
-matches_subset <- matches_clean %>% 
-  head(10)
+column_names <- c("team_key", "alliance_color", "comp_level", "event_key", "key", "winning_alliance", "blue_score", "red_scoare")
+data_by_team <- data.frame(matrix(ncol = length(column_names), nrow = 0))
 
-for (y in 1:nrow(matches_subset)) {
-  print(y)
-  expand_df(y)
+rows <- nrow(matches_clean)
+for (row in 1:rows) {
+  new_data <- expand_df(row)
+  data_by_team <- rbind(data_by_team, new_data)
+  print(row)
 }
+
+write_csv(data_by_team, "team_match_data.csv")
 
 
 
